7b76508c32e406025a88a0d36d72d1196cd196f5
jags1429 <pothaljagruti@gmail.com>
2025-07-19 23:17:13 +0530

    commit new changes

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".fade-in-section");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+      } else {
+        entry.target.classList.remove("is-visible");
+      }
+    });
+  }, { threshold: 0.3 });
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
